48849c47af850dbb5f8b5484685dff8015d87cbc
Shwei <portal.gamy@gmail.com>
2026-05-16 17:45:31 +0300

    Add pattern matching to lw5 fn signatures

--- Crypto/LW5/src/diffie_hellman.rs
+++ Crypto/LW5/src/diffie_hellman.rs
@@ -19,12 +19,12 @@ pub fn generate_private_key(params: &PublicParams) -> u32 {
     rand::random_range(2..params.p-2)
 }
 
-pub fn generate_public_key(params: &PublicParams, private_key: u32) -> u32 {
-    power_modulo(params.g, private_key, params.p)
+pub fn generate_public_key(&PublicParams { g, p }: &PublicParams, private_key: u32) -> u32 {
+    power_modulo(g, private_key, p)
 }
 
-pub fn compute_shared_secret(params: &PublicParams, my_private_key: u32, other_public_key: u32) -> u32 {
-    power_modulo(other_public_key, my_private_key, params.p)
+pub fn compute_shared_secret(&PublicParams {g: _g, p}: &PublicParams, my_private_key: u32, other_public_key: u32) -> u32 {
+    power_modulo(other_public_key, my_private_key, p)
 }
 
 
